2c20b8615afbf1410a85aaa2b45cd08247a89a7e
Đạt Lee <ledat@smartwave247.com>
2024-05-24 10:25:32 +0700

    + setting panel

--- Assets/Scripts/HomeSceneButtonController.cs
+++ Assets/Scripts/HomeSceneButtonController.cs
@@ -11,12 +11,12 @@ public class HomeSceneButtonController : MonoBehaviour {
     //[SerializeField] TextMeshProUGUI personalProfileText;
     //[SerializeField] TextMeshProUGUI hasSetedProfileText;
     [SerializeField] RectTransform dailyRewardBoard;
-    public GameObject purchasePanel, shopPanel, soundEffectOn, soundEffectOff, soundEffectIconOn, soundEffectIconOff;
-    public GameObject musicEffectOn, musicEffectOff, musicEffectIconOn, musicEffectIconOff;
-    public GameObject vibrateEffectOn, vibrateEffectOff, vibrateEffectIconOn, vibrateEffectIconOff;
-    public GameObject[] onGoingIcons;
-    public GameObject[] lockIcons;
-    public GameObject[] finishedIcons;
+    public GameObject purchasePanel, shopPanel, soundEffectOn, soundEffectOff;
+    public GameObject musicEffectOn, musicEffectOff;
+    public GameObject vibrateEffectOn, vibrateEffectOff;
+    //public GameObject[] onGoingIcons;
+    //public GameObject[] lockIcons;
+    //public GameObject[] finishedIcons;
     public int goldenTicketNumber;
     public TextMeshProUGUI goldenTicketNumberText;
 
@@ -39,13 +39,13 @@ public class HomeSceneButtonController : MonoBehaviour {
 
     private void Update() {
         int levelPassed = PlayerPrefs.GetInt(StringsTextManager.LevelPassed, 0);
-        if (musicEffectIconOn.activeSelf)
+        if (musicEffectOn.activeSelf)
             PlayerPrefs.SetInt(StringsTextManager.MusicEffectKey, 1);
-        if (soundEffectIconOn.activeSelf)
+        if (soundEffectOn.activeSelf)
             PlayerPrefs.SetInt(StringsTextManager.SoundEffectKey, 1);
-        if (vibrateEffectIconOn.activeSelf)
+        if (vibrateEffectOn.activeSelf)
             PlayerPrefs.SetInt(StringsTextManager.VibrateEffectKey, 1);
-        UpdateLevelIcons(levelPassed);
+        //UpdateLevelIcons(levelPassed);
         //if (profilePanel.activeSelf) {
         //    hasSetedProfileText.text = PlayerPrefs.GetString(StringsManager.PersonalInformationText);
         //}
@@ -55,48 +55,44 @@ public class HomeSceneButtonController : MonoBehaviour {
         }
     }
 
-    private void UpdateLevelIcons(int levelPassed) {
-        for (int i = 0; i < onGoingIcons.Length; i++) {
-            onGoingIcons[i].SetActive(i == levelPassed - 1);
-            lockIcons[i].SetActive(i >= levelPassed);
-            finishedIcons[i].SetActive(i < levelPassed - 1);
-        }
-    }
+    //private void UpdateLevelIcons(int levelPassed) {
+    //    for (int i = 0; i < onGoingIcons.Length; i++) {
+    //        onGoingIcons[i].SetActive(i == levelPassed - 1);
+    //        lockIcons[i].SetActive(i >= levelPassed);
+    //        finishedIcons[i].SetActive(i < levelPassed - 1);
+    //    }
+    //}
 
     public void SoundBtn() {
-        ToggleState(soundEffectOn, soundEffectOff, soundEffectIconOn, soundEffectIconOff, StringsTextManager.SoundEffectKey);
+        ToggleState(soundEffectOn, soundEffectOff, StringsTextManager.SoundEffectKey);
     }
 
     public void MusicBtn() {
-        ToggleState(musicEffectOn, musicEffectOff, musicEffectIconOn, musicEffectIconOff, StringsTextManager.MusicEffectKey);
+        ToggleState(musicEffectOn, musicEffectOff, StringsTextManager.MusicEffectKey);
     }
 
     public void VibrateBtn() {
-        ToggleState(vibrateEffectOn, vibrateEffectOff, vibrateEffectIconOn, vibrateEffectIconOff, StringsTextManager.VibrateEffectKey);
+        ToggleState(vibrateEffectOn, vibrateEffectOff, StringsTextManager.VibrateEffectKey);
     }
 
-    private void ToggleState(GameObject onObj, GameObject offObj, GameObject iconOn, GameObject iconOff, string key) {
+    private void ToggleState(GameObject onObj, GameObject offObj, string key) {
         bool currentState = onObj.activeSelf;
         onObj.SetActive(!currentState);
         offObj.SetActive(currentState);
-        iconOn.SetActive(!currentState);
-        iconOff.SetActive(currentState);
         PlayerPrefs.SetInt(key, currentState ? 0 : 1);
         PlayerPrefs.Save();
     }
 
     private void LoadState() {
-        LoadToggleState(soundEffectOn, soundEffectOff, soundEffectIconOn, soundEffectIconOff, StringsTextManager.SoundEffectKey);
-        LoadToggleState(musicEffectOn, musicEffectOff, musicEffectIconOn, musicEffectIconOff, StringsTextManager.MusicEffectKey);
-        LoadToggleState(vibrateEffectOn, vibrateEffectOff, vibrateEffectIconOn, vibrateEffectIconOff, StringsTextManager.VibrateEffectKey);
+        LoadToggleState(soundEffectOn, soundEffectOff, StringsTextManager.SoundEffectKey);
+        LoadToggleState(musicEffectOn, musicEffectOff, StringsTextManager.MusicEffectKey);
+        LoadToggleState(vibrateEffectOn, vibrateEffectOff, StringsTextManager.VibrateEffectKey);
     }
 
-    private void LoadToggleState(GameObject onObj, GameObject offObj, GameObject iconOn, GameObject iconOff, string key) {
+    private void LoadToggleState(GameObject onObj, GameObject offObj, string key) {
         bool state = PlayerPrefs.GetInt(key, 1) == 1;
         onObj.SetActive(state);
         offObj.SetActive(!state);
-        iconOn.SetActive(state);
-        iconOff.SetActive(!state);
     }
 
     public void LevelPanelAppear(RectTransform panel) {
@@ -113,18 +109,18 @@ public class HomeSceneButtonController : MonoBehaviour {
         SceneManager.LoadScene("PlayScene");
     }
 
-    public void LoadLevel(int level) {
-        if (level < 1 || level > onGoingIcons.Length) {
-            return;
-        }
+    //public void LoadLevel(int level) {
+    //    if (level < 1 || level > onGoingIcons.Length) {
+    //        return;
+    //    }
 
-        if (onGoingIcons[level - 1].activeSelf || finishedIcons[level - 1].activeSelf) {
-            PlayerPrefs.SetInt(StringsTextManager.PlayButtonLoadScene, 0);
-            PlayerPrefs.SetInt(StringsTextManager.LevelButtonLoadScene, 1);
-            PlayerPrefs.SetInt(StringsTextManager.LevelButtonIdx, level);
-            SceneManager.LoadScene("PlayScene");
-        }
-    }
+    //    if (onGoingIcons[level - 1].activeSelf || finishedIcons[level - 1].activeSelf) {
+    //        PlayerPrefs.SetInt(StringsTextManager.PlayButtonLoadScene, 0);
+    //        PlayerPrefs.SetInt(StringsTextManager.LevelButtonLoadScene, 1);
+    //        PlayerPrefs.SetInt(StringsTextManager.LevelButtonIdx, level);
+    //        SceneManager.LoadScene("PlayScene");
+    //    }
+    //}
 
     public void PopSound() {
         PlaySoundEffectManager.instance.audioEffectSource.PlayOneShot(PlaySoundEffectManager.instance.popEffectSound);
